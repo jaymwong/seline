e0e9206e4f967ae9ed18697c32418945fffe1f3d
Jay M. Wong <jay@southieautonomyworks.com>
2018-01-20 23:54:54 -0500

    More development on publishing the transformed pointclouds

--- include/seline.h
+++ include/seline.h
@@ -6,10 +6,14 @@
 
 #include <Eigen/Core>
 
+#include <pcl/common/transforms.h>
 #include <pcl/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/PCLPointCloud2.h>
+#include <sensor_msgs/PointCloud2.h>
+
 
 #define kDefaultLoopRate 1
 
@@ -24,7 +28,13 @@ class Seline{
     ros::NodeHandle nh_;
 
     std::string seline_pkg_dir_;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr original_model_cloud_; // cloud as loaded from .pcd; no transform applied
+
+    ros::Publisher pub_original_cloud_, pub_transformed_cloud_;
+
+    std::string camera_optical_frame_;
+
+    void publishPointCloudXYZ(ros::Publisher pub, pcl::PointCloud<pcl::PointXYZ> &pcl_cloud, std::string frame_id);
 };
 
 #endif
--- src/seline.cc
+++ src/seline.cc
@@ -6,24 +6,47 @@ Seline::Seline(){
   seline_pkg_dir_ = ros::package::getPath("seline");
   std::string path_to_model = seline_pkg_dir_ + "/models/" + ee_model;
 
-  model_cloud_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> (path_to_model, *model_cloud_) != kModelLoadErrorCode){
-    std::cout << "Successfully loaded file " << ee_model << " (" << model_cloud_->size () << " points)\n";
+  original_model_cloud_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> (path_to_model, *original_model_cloud_) != kModelLoadErrorCode){
+    std::cout << "Successfully loaded file " << ee_model << " (" << original_model_cloud_->size () << " points)\n";
   }
 
-  //pcl::PointCloud<pcl::PointXYZ>
+  pub_original_cloud_ = nh_.advertise<sensor_msgs::PointCloud2>("/seline/original_cloud", 1);
+  pub_transformed_cloud_ = nh_.advertise<sensor_msgs::PointCloud2>("/seline/transformed_cloud", 1);
+
+  camera_optical_frame_ = "camera_rgb_optical_frame";
 
   Eigen::Affine3f seed_transform;
   seed_transform.matrix() = Eigen::MatrixXf::Identity(4, 4);
 
   // TODO: A test... display the original pointcloud a little bit...
-  seed_transform.translation() = Eigen::Vector3f(1,2,3);
+  seed_transform.translation() = Eigen::Vector3f(1.0, 0.2, 1.0);
 
   std::cout << seed_transform.matrix() << "\n";
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::transformPointCloud(*original_model_cloud_, *transformed_cloud, seed_transform);
+
+  std::cout << "Transformed cloud: " << transformed_cloud->size() << " points\n";
+
+
+  publishPointCloudXYZ(pub_original_cloud_, *original_model_cloud_, camera_optical_frame_);
+  publishPointCloudXYZ(pub_transformed_cloud_, *transformed_cloud, camera_optical_frame_);
+
 
 }
 Seline::~Seline(){
 
+}
+
+void Seline::publishPointCloudXYZ(ros::Publisher pub, pcl::PointCloud<pcl::PointXYZ> &pcl_cloud, std::string frame_id){
+  pcl::PCLPointCloud2 pcl_pc2;
+  pcl::toPCLPointCloud2(pcl_cloud, pcl_pc2);
+
+  sensor_msgs::PointCloud2 cloud_msg;
+  pcl_conversions::fromPCL(pcl_pc2, cloud_msg);
+  cloud_msg.header.frame_id = frame_id;
+  pub.publish(cloud_msg);
 }
 
 int main(int argc, char** argv){
