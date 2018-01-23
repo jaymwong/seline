b8c2d4aadb0e384f0573e19498e0aabee5d74d82
Jay M. Wong <jay@southieautonomyworks.com>
2018-01-23 08:18:47 -0500

    Initial ICP registration implmentation

--- include/seline.h
+++ include/seline.h
@@ -15,12 +15,18 @@
 #include <sensor_msgs/PointCloud2.h>
 
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>
+#include <pcl/kdtree/kdtree_flann.h>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
 
 #define kDefaultLoopRate 1
 
 #define kModelLoadErrorCode -1
 #define kDownSampleModelLeafSize 0.005
+#define kDefaultPointCloudLeafSize 0.01
 
 class Seline{
   public:
@@ -36,13 +42,28 @@ class Seline{
     pcl::PointCloud<pcl::PointXYZ>::Ptr original_model_cloud_; // cloud as loaded from .pcd; no transform applied
     pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud_;
 
+    pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_xyz_;
+
+
+    pcl::PointXYZ seeded_point_;
+    bool has_seed_;
+    Eigen::Matrix4f transformation_matrix_; // camera -> ee
+
     ros::Publisher pub_original_cloud_, pub_transformed_cloud_;
+    ros::Subscriber sub_point_cloud_;
 
     std::string camera_optical_frame_;
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud_, target_cloud_; // the model cloud, and the scene cloud
 
+    void downsampleInitialModelCloud();
+    void processSeed(Eigen::Matrix4f matrix);
     void publishPointCloudXYZ(ros::Publisher pub, pcl::PointCloud<pcl::PointXYZ> &pcl_cloud, std::string frame_id);
+    void doIterativeRegistration(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud);
+
+
+    void inputCloudCallback(const sensor_msgs::PointCloud2ConstPtr& input);
+
 };
 
 #endif
--- src/seline.cc
+++ src/seline.cc
@@ -13,46 +13,46 @@ Seline::Seline(){
 
   pub_original_cloud_ = nh_.advertise<sensor_msgs::PointCloud2>("/seline/original_cloud", 1);
   pub_transformed_cloud_ = nh_.advertise<sensor_msgs::PointCloud2>("/seline/transformed_cloud", 1);
+  sub_point_cloud_ = nh_.subscribe("/camera/depth_registered/points", 1, &Seline::inputCloudCallback, this);
+
+  downsampleInitialModelCloud();
 
   camera_optical_frame_ = "camera_rgb_optical_frame";
-
-  Eigen::Affine3f seed_transform;
-  seed_transform.matrix() = Eigen::MatrixXf::Identity(4, 4);
-
-
-  pcl::PointCloud<pcl::PointNormal>::Ptr pn (new pcl::PointCloud<pcl::PointNormal>);
-  copyPointCloud(*original_model_cloud_, *pn);
-
-  pcl::VoxelGrid<pcl::PointNormal> grid;
-  grid.setLeafSize (0.0075, 0.0075, 0.0075);
-  grid.setInputCloud (pn);
-  grid.filter (*pn);
-  copyPointCloud(*pn, *original_model_cloud_);
-
-
-
-
-  // TODO: A test... display the original pointcloud a little bit...
-  seed_transform.translation() = Eigen::Vector3f(1.0, 0.2, 1.0);
-
-  std::cout << seed_transform.matrix() << "\n";
-
-  transformed_cloud_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::transformPointCloud(*original_model_cloud_, *transformed_cloud_, seed_transform);
-
-  std::cout << "Transformed cloud: " << transformed_cloud_->size() << " points\n";
-
-
-  publishPointCloudXYZ(pub_original_cloud_, *original_model_cloud_, camera_optical_frame_);
-  publishPointCloudXYZ(pub_transformed_cloud_, *transformed_cloud_, camera_optical_frame_);
+  transformation_matrix_ = Eigen::MatrixXf::Identity(4, 4);
+  has_seed_ = false;
 
   source_cloud_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
   target_cloud_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+  publishPointCloudXYZ(pub_original_cloud_, *original_model_cloud_, camera_optical_frame_);
 
 
 }
 Seline::~Seline(){
 
+}
+
+
+void Seline::inputCloudCallback(const sensor_msgs::PointCloud2ConstPtr& input){
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  pcl::PCLPointCloud2* input_cloud_pcl = new pcl::PCLPointCloud2;
+  pcl::PCLPointCloud2ConstPtr cloudPtr(input_cloud_pcl);
+  pcl::PCLPointCloud2 cloud_filtered;
+
+  pcl_conversions::toPCL(*input, *input_cloud_pcl);
+  sor.setInputCloud(cloudPtr);
+  sor.setLeafSize(kDefaultPointCloudLeafSize, kDefaultPointCloudLeafSize, kDefaultPointCloudLeafSize);
+  sor.filter(cloud_filtered);
+  pcl::fromPCLPointCloud2(cloud_filtered, *input_cloud_xyz_);
+}
+
+void Seline::downsampleInitialModelCloud(){
+  pcl::PointCloud<pcl::PointNormal>::Ptr pn (new pcl::PointCloud<pcl::PointNormal>);
+  copyPointCloud(*original_model_cloud_, *pn);
+  pcl::VoxelGrid<pcl::PointNormal> grid;
+  grid.setLeafSize (kDownSampleModelLeafSize, kDownSampleModelLeafSize, kDownSampleModelLeafSize);
+  grid.setInputCloud (pn);
+  grid.filter (*pn);
+  copyPointCloud(*pn, *original_model_cloud_);
 }
 
 void Seline::publishPointCloudXYZ(ros::Publisher pub, pcl::PointCloud<pcl::PointXYZ> &pcl_cloud, std::string frame_id){
@@ -62,13 +62,60 @@ void Seline::publishPointCloudXYZ(ros::Publisher pub, pcl::PointCloud<pcl::Point
   sensor_msgs::PointCloud2 cloud_msg;
   pcl_conversions::fromPCL(pcl_pc2, cloud_msg);
   cloud_msg.header.frame_id = frame_id;
-  std::cout << "Publish in " << frame_id << "\n";
   pub.publish(cloud_msg);
 }
 
+void Seline::processSeed(Eigen::Matrix4f matrix){
+    Eigen::Affine3f seed_transform;
+    seed_transform.matrix() = matrix;
+
+    auto translation = seed_transform.translation();
+    pcl::PointXYZ search_point;
+    search_point.x = translation[0];
+    search_point.y = translation[1];
+    search_point.z = translation[2];
+
+    // std::cout << seed_transform.matrix() << "\n";
+
+    // Transform the original cloud using the seed transform
+    transformed_cloud_ = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::transformPointCloud(*original_model_cloud_, *transformed_cloud_, seed_transform);
+    std::cout << "Transformed cloud: " << transformed_cloud_->size() << " points\n";
+    publishPointCloudXYZ(pub_transformed_cloud_, *transformed_cloud_, camera_optical_frame_);
+}
+
+// Continuously calls ICP and uses the solution as a seed for the next
+void Seline::doIterativeRegistration(pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud){
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+
+  double kIterations = 1;
+  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  icp.setMaximumIterations (kIterations);
+  icp.setInputSource (source_cloud);
+  icp.setInputTarget (target_cloud);
+  icp.align (*cloud_out);
+  icp.setMaximumIterations (1);  // We set this variable to 1 for the next time we will call .align () function
+
+  if (icp.hasConverged ())
+    {
+      std::cout << "\nICP has converged, score is " << icp.getFitnessScore () << std::endl;
+      //auto transformation_matrix = icp.getFinalTransformation ().cast<double>() * transformation_matrix;
+      //std::cout << transformation_matrix << "\n";
+      //*cloud_model = *cloud_out;
+    }
+    else
+    {
+      PCL_ERROR ("\nICP has not converged.\n");
+    }
+
+}
+
 void Seline::runOnce(){
+
+  // Debugging push out the original point cloud and the transformed point cloud
   publishPointCloudXYZ(pub_original_cloud_, *original_model_cloud_, camera_optical_frame_);
-  publishPointCloudXYZ(pub_transformed_cloud_, *transformed_cloud_, camera_optical_frame_);
+  // publishPointCloudXYZ(pub_transformed_cloud_, *transformed_cloud_, camera_optical_frame_);
 }
 
 int main(int argc, char** argv){
