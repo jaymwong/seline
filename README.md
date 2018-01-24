f03aa8e80598075ae0d6874aac98ccecda0e820b
Jay M. Wong <jay@southieautonomyworks.com>
2018-01-24 15:08:19 -0500

    Generalizing processing script

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ The purpose of `seline` is to use model-based pose estimation to estimate the tr
  1. The robot has sufficiently good forward kinematics.
  2. A decent seed is available, meaning that the current URDF for the robot must have a connected tree that has both the camera and the base of the robot.
  3. A `.pcd` model of the end effector is available.
- 
+
 ## Procedure
 How `seline` works is as follows. The forward kinematics of the robot is used to obtain a seed location of where the robot believes its end effector is relative to the camera. An epsilon ball is created around this initial seed. We go through the raw point cloud and obtain all points in the scene within this epsilon ball and treat this as the target cloud for registration. We then seed the model (the cloud corresponding to the end effector) at where pose given by forward kinematics and do point to point ICP between the end effector model and the segmented scene cloud. Doing so obtains an estimated camera to end effector transform which is used to estimate where the transformation for base of the robot.
- 
+
 ## Requirements
 The following are dependencies for `seline`,
  * Robot Operating System (ROS), Point Cloud Library (PCL), Eigen
@@ -16,12 +16,26 @@ The following are dependencies for `seline`,
 
 ## Usage
 An example configuration file is in `seline/config`. For your setting, create a new configuration file and edit `seline/launch/seline.launch` to use this new configuration file. Then simply do,
-```roslaunch seline seline.launch```
+```
+roslaunch seline seline.launch
+```
 Finally, to obtain the new camera to world transformation, do
 ```
 cd seline/scripts; python process_data.py;
 ```
+This will print out the transformation in URDF friendly format. For example, 
+```
+<link name="camera_link"/>
+<joint name="camera_to_robot" type="fixed">
+  <parent link="camera_link"/>
+  <child link="world"/>
+  <origin xyz="1.5460137711 -0.0991888599915 0.295267672157" rpy="-0.546406834079 -0.00742609072367 1.53930854082" />
+</joint>
+```
 __TODO:__ We are currently writing a procedure to automatically send the arm to various locations in the scene, for each location estimate the new world frame, and perform a regression over the set of estimated frames.
+
+
+NOTE, in reality the `world_frame` is actually the robot's `base_link`; this README is using them as if they are interchangable since `seline` was originally designed for a robot with a fixed base.  
 
 ## Examples
 Given the observed segmented scene pointcloud in green, `seline` estimates the end effector pose using the forward kinematics as a seed (blue) and performs point to point ICP with the segmented scene cloud (green). This results in the estimated camera to end effector transformation. Since we know the forward kinematics, we can compute the end effector to world tranform (as described by the robot's URDF), and back out the estimated new world frame.
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -8,28 +8,42 @@ class SelineProcessData:
         self.sub_tf = rospy.Subscriber('/seline/est_world_frame', HomogeneousTransform, self.tf_callback)
         self.listener = tf.TransformListener()
 
+        self.camera_optical_frame = rospy.get_param("/seline/camera_optical_frame")
+        self.world_frame = rospy.get_param("/seline/world_frame")
+        self.camera_link_name = rospy.get_param("/seline/camera_link_name")
+
     def tf_callback(self, msg):
-        (xyz, quat) = self.listener.lookupTransform('/camera_rgb_optical_frame', '/camera_link', rospy.Time(0))
+        (xyz, quat) = self.listener.lookupTransform(self.camera_optical_frame, self.camera_link_name, rospy.Time(0))
 
         optical_to_base = transf.translation_matrix([xyz[0], xyz[1], xyz[2]])
         optical_to_base = np.dot(optical_to_base, transf.quaternion_matrix([quat[3], quat[0], quat[1], quat[2]]))
-        print optical_to_base
+        #print optical_to_base
 
         cam_to_world = homogeneous_vector_to_matrix(msg.transform)
         world_to_cam = transf.inverse_matrix(cam_to_world)
 
-        broadcast_matrix_as_tf(cam_to_world, 'camera_rgb_optical_frame', 'est_world')
-        broadcast_matrix_as_tf(world_to_cam, 'world', 'est_camera_optical_frame')
+        broadcast_matrix_as_tf(cam_to_world, self.camera_optical_frame, 'est_world')
+        broadcast_matrix_as_tf(world_to_cam, self.world_frame, 'est_camera_optical_frame')
 
         world_to_cam_base = np.dot(world_to_cam, optical_to_base)
-        broadcast_matrix_as_tf(world_to_cam_base, 'world', 'est_camera_base_link')
+        broadcast_matrix_as_tf(world_to_cam_base, self.world_frame, 'est_camera_base_link')
 
-        world_to_cam_base = transf.inverse_matrix(world_to_cam_base)
-        print '*** Est. world to camera frame ***'
-        xyz = transf.translation_from_matrix(world_to_cam_base)
-        rpy = transf.euler_from_matrix(world_to_cam_base)
-        print 'xyz:', xyz[0], ' ', xyz[1], ' ', xyz[2]
-        print 'rpy:', rpy[0], ' ', rpy[1], ' ', rpy[2]
+        # Obtain the camera to world transform, sometimes in URDFs we want the camera_link
+        # to be the root of the tree as described by the URDF
+        cam_base_to_world = transf.inverse_matrix(world_to_cam_base)
+        print '\n*** Est. world to camera frame ***'
+        xyz = transf.translation_from_matrix(cam_base_to_world)
+        rpy = transf.euler_from_matrix(cam_base_to_world)
+        # print 'xyz:', xyz[0], ' ', xyz[1], ' ', xyz[2]
+        # print 'rpy:', rpy[0], ' ', rpy[1], ' ', rpy[2]
+
+
+        print '<link name=\"'+self.camera_link_name+'\"/>'
+        print '<joint name=\"camera_to_robot\" type=\"fixed\">'
+        print '  <parent link=\"'+self.camera_link_name+'\"/>'
+        print '  <child link=\"'+self.world_frame+'\"/>'
+        print '  <origin xyz=\"'+str(xyz[0])+' '+str(xyz[1])+' '+str(xyz[2])+'\" rpy=\"'+str(rpy[0])+' '+str(rpy[1])+' '+str(rpy[2])+'\" />'
+        print '</joint>'
 
 
 
